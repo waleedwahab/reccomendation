8cd28ac7179fcec14d7423da0365871275f50380
unknown <walees.wahab321@gmail.com>
2023-07-08 14:08:54 -0700

    Update README.md

--- src/layout/Unauthorized.js
+++ src/layout/Unauthorized.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 const Unauthorized = ()=>
 {
 
-    
+
     return()=>
     <>
-     <h1 style={{ color: 'red', textAlign: 'center' }}>please login to use these routes </h1>
+     <h1 style={{ color: 'blue', textAlign: 'center' }}>please login to use these routes </h1>
      </>
     
  
